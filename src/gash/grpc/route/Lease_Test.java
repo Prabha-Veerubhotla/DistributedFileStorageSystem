91a1c89ba38f444d267a3df5570cb04f3b12d84c
PrabhaVeerubhotla <vvlprabha@gmail.com>
2019-03-04 16:12:23 -0800

    changes in lease test
    Signed-off-by: PrabhaVeerubhotla <vvlprabha@gmail.com>

--- src/gash/grpc/route/Lease_Test.java
+++ src/gash/grpc/route/Lease_Test.java
@@ -7,22 +7,29 @@ public class Lease_Test {
 
         public static void main(String args[]) {
             // monitor a single file
-            TimerTask task = new Lease_Changes_Monitor( new File("/Users/prabha/Desktop/SEM_2019/275/Labs/275-project1-demo1/lease.txt") ) {
-                protected void onChange( File file ) {
+            System.out.println("Monitoring Lease file");
+        }
+
+        public static void monitorLease() {
+            TimerTask task = new Lease_Changes_Monitor(new File("/var/lib/dhcpd/dhcpd.lease")) {
+                protected void onChange(File file) {
                     // here we code the action on a change
-                    System.out.println( "File "+ file.getName() +" have change !" );
+                    System.out.println("File " + file.getName() + " have change !");
                     try {
-                    new ProcessBuilder("grep -o '[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}' /var/lib/dhcpd/dhcpd.leases").start();
+                        Process p = new ProcessBuilder("home/vinod/cmpe275/labs/untitled/src-dhcp-ip-list/fetch_ip.sh\"").start();
+                        BufferedReader reader1 = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                        String line1 = null;
+                        System.out.println("Printing all the ips1: ");
+                        while ((line1 = reader1.readLine()) != null) {
+                            System.out.println(line1 + "\n");
+                        }
                     } catch (IOException io) {
                         System.out.println("io exception");
                     }
-
                 }
             };
-
             Timer timer = new Timer();
             // repeat the check every second
-            timer.schedule( task , new Date(), 1000 );
+            timer.schedule(task, new Date(), 1000);
         }
-
 }
